df6952d4ed51ebf375df29f9b7f3007fd37cb8fc
Swartdogs Laptop 1 <SwartdogsLaptop1@gmail.com>
2019-01-21 13:12:59 -0600

    Created CmdElevatorSetPosition command

---
+++ src/main/cpp/commands/CmdElevatorSetPosition.cpp
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+#include "commands/CmdElevatorSetPosition.h"
+
+CmdElevatorSetPosition::CmdElevatorSetPosition(Elevator::ElevatorPosition position) {
+  m_elevatorPosition = position;
+}
+
+// Called just before this Command runs the first time
+void CmdElevatorSetPosition::Initialize() {
+  Robot::m_elevator.SetElevatorPosition(m_elevatorPosition);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void CmdElevatorSetPosition::Execute() {}
+
+// Make this return true when this Command no longer needs to run execute()
+bool CmdElevatorSetPosition::IsFinished() { return true; }
+
+// Called once after isFinished returns true
+void CmdElevatorSetPosition::End() {}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void CmdElevatorSetPosition::Interrupted() {}
