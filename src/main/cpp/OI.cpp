0b667c520089a4fe55c931dbb7d979707b247b02
Collin McIntyre <crm11396@gmail.com>
2019-02-05 17:54:31 -0600

    Removed  non-drive items from OI

--- src/main/cpp/OI.cpp
+++ src/main/cpp/OI.cpp
@@ -1,13 +1,8 @@
 #include "OI.h"
 #include <frc/WPILib.h>
-#include "commands/GrpClimb.h"
-#include "commands/CmdCancelClimb.h"
-#include "subsystems/Elevator.h"
 
 OI::OI() {
-  m_InternalLevel2.WhenPressed    (new GrpClimb(Elevator::epLevel2));
-  m_InternalLevel3.WhenPressed    (new GrpClimb(Elevator::epLevel3));
-  m_buttonClimbCancel.WhenPressed (new CmdCancelClimb());
+
 }
 
 double OI::ApplyDeadband(double joystickValue, double deadband) {
@@ -22,23 +17,10 @@ double OI::ApplyDeadband(double joystickValue, double deadband) {
   return 0;
 }
 
-double OI::GetArmJoystickX() {
-  return m_armJoystick.GetX();
-}
-
-double OI::GetArmJoystickY() {
-  return -m_armJoystick.GetY();
-}
-
 double OI::GetDriveJoystickX(){
   return m_driveJoystick.GetX();
 }
 
 double OI::GetDriveJoystickY(){
   return -m_driveJoystick.GetY();
-}
-
-void OI::Periodic() {
-  m_InternalLevel2.SetPressed(m_buttonClimbEnable.Get() && m_buttonClimbLevel2.Get());
-  m_InternalLevel3.SetPressed(m_buttonClimbEnable.Get() && m_buttonClimbLevel3.Get());
 }
