d51ccbbe040f320975da985e5b271823da70842a
Collin McIntyre <crm11396@gmail.com>
2019-02-04 20:27:03 -0600

    Removed non-logging items from RobotMap

--- src/main/include/RobotMap.h
+++ src/main/include/RobotMap.h
@@ -1,88 +1,9 @@
 #pragma once
 
 typedef enum {
-    csRun,
-    csSkip,
-    csDone,
-    csCancel,
-    csTimedOut, 
-    csContinue
-} CommandStatus;
-
-typedef enum {
     rmInit,					// Robot Mode 		(Dashboard, RobotLog)
 	rmDisabled,
     rmAutonomous,
     rmTeleop,
     rmTest
 } RobotMode;
-
-typedef enum {
-    pwmDriveLeft1,
-    pwmDriveLeft2,
-    pwmDriveRight1,
-    pwmDriveRight2,
-    pwmElevatorMotor,
-    pwmFootMotor,
-    pwmShoulder,
-    pwmWrist
-} RoboRioPwm;
-
-typedef enum {
-    dioEncoderLeftChannelA,
-    dioEncoderLeftChannelB,
-    dioEncoderRightChannelA,
-    dioEncoderRightChannelB,
-    dioFrontSensor,
-    dioRearSensor,
-    dioSonarLeftEcho,
-    dioSonarLeftPing,
-    dioSonarRightEcho,
-    dioSonarRightPing
-} RoboRioDio;
-
-typedef enum {
-    aioElevatorPot,
-    aioShoulderPot,
-    aioWristPot
-} RoboRioAio;
-
-typedef enum {
-    solShifter,
-    solFoot
-} RoboRioSol;
-
-typedef enum {
-    rsNone
-} RobotStatus;
-
-typedef enum {
-    rvNone
-} RobotValue;
-
-typedef enum { 
-    dbRunPid
-} DashButton;
-
-typedef enum{ 
-    dvPidSelect,
-    dvPidSetpoint,
-    dvPidMaxOut,
-    dvPthreshold,
-    dvPabove,
-    dvPbelow,
-    dvIthreshold,
-    dvIabove,
-    dvIbelow,
-    dvDthreshold,
-    dvDabove,
-    dvDbelow,
-    dvElevOffset,
-    dvElevMin,
-    dvElevMax,
-    dvSensorMin,
-    dvElevRetracted,
-    dvElevLevel2,
-    dvElevLevel3,
-    dvDriveRatio
-} DashValue;
