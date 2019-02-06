23c4295cf0436fcf35f568b35ed49aca7cdd2465
Collin McIntyre <crm11396@gmail.com>
2019-02-05 18:16:22 -0600

    Removed non-drive items from RobotMap

--- src/main/include/RobotMap.h
+++ src/main/include/RobotMap.h
@@ -10,22 +10,10 @@ typedef enum {
 } CommandStatus;
 
 typedef enum {
-    rmInit,					// Robot Mode 		(Dashboard, RobotLog)
-	rmDisabled,
-    rmAutonomous,
-    rmTeleop,
-    rmTest
-} RobotMode;
-
-typedef enum {
     pwmDriveLeft1,
     pwmDriveLeft2,
     pwmDriveRight1,
-    pwmDriveRight2,
-    pwmElevatorMotor,
-    pwmFootMotor,
-    pwmShoulder,
-    pwmWrist
+    pwmDriveRight2
 } RoboRioPwm;
 
 typedef enum {
@@ -33,8 +21,6 @@ typedef enum {
     dioEncoderLeftChannelB,
     dioEncoderRightChannelA,
     dioEncoderRightChannelB,
-    dioFrontSensor,
-    dioRearSensor,
     dioSonarLeftEcho,
     dioSonarLeftPing,
     dioSonarRightEcho,
@@ -42,83 +28,5 @@ typedef enum {
 } RoboRioDio;
 
 typedef enum {
-    aioElevatorPot,
-    aioShoulderPot,
-    aioWristPot
-} RoboRioAio;
-
-typedef enum {
-    solShifter,
-    solFoot
+    solShifter
 } RoboRioSol;
-
-typedef enum {
-    rsNone
-} RobotStatus;
-
-typedef enum {
-    rvDriveGyro,
-    rvDriveEncoderL,
-    rvDriveEncoderR,
-    rvDriveAmpsLeft1,
-    rvDriveAmpsLeft2,
-    rvDriveAmpsRight1,
-    rvDriveAmpsRight2,
-    rvElevatorPosition,
-    rvElevatorSetpoint,
-    rvElevatorFoot,
-    rvElevatorAmps,
-    rvShoulderPosition,
-    rvShoulderSetpoint,
-    rvShoulderAmps,
-    rvWristPosition,
-    rvWristSetpoint,
-    rvWristAmps
-} RobotValue;
-
-typedef enum { 
-    dbRunPid
-} DashButton;
-
-typedef enum{ 
-    dvPidSelect,
-    dvPidSetpoint,
-    dvPidMaxOut,
-    dvPthreshold,
-    dvPabove,
-    dvPbelow,
-    dvIthreshold,
-    dvIabove,
-    dvIbelow,
-    dvDthreshold,
-    dvDabove,
-    dvDbelow,
-    dvElevOffset,
-    dvElevMin,
-    dvElevMax,
-    dvSensorMin,
-    dvElevRetracted,
-    dvElevLevel2,
-    dvElevLevel3,
-    dvDriveRatio,
-    dvShoulderMin, 
-    dvShoulderMax,
-    dvShoulderOffset,
-    dvShoulderTravel,
-    dvShoulderFloor,
-    dvShoulderCargo,
-    dvShoulderRocketLow,
-    dvShoulderRocketMid,
-    dvShoulderRocketHigh,
-    dvShoulderModify,
-    dvWristMin,
-    dvWristMax,
-    dvWristOffset,
-    dvWristTravel,
-    dvWristFloor,
-    dvWristCargo,
-    dvWristRocketLow,
-    dvWristRocketMid,
-    dvWristRocketHigh,
-    dvWristModify
-} DashValue;
